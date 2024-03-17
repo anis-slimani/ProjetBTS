bfb063456bc5ba833c44f33e1c8d82468e026b27
Anis Slimani <anis.sliimani@gmail.com>
2024-03-17 15:19:26 +0100

    N'afficher pas le tableau d'hackathons dans l'accueil si il est vide

--- _code/views/pages/view_accueil.php
+++ _code/views/pages/view_accueil.php
@@ -23,28 +23,33 @@
 		?>
 	</nav>
 	<main>
-		<h2>Accueil : à faire !</h2>
-		<section>
-			<h3>Voici la liste des nouveaux hackathons : incrivez vous !</h2>
-		</section>
-		<table border>
-			<tr>
-				<th>id</th>
-				<th>nom</th>
-				<th>date de début</th>
-			</tr>
-			<?php foreach ($hackathons as $hackathon) { ?>
-				<tr>
-					<td><?php echo $hackathon->getId() ?></td>
-					<td><?php echo $hackathon->getNom() ?></td>
-					<td><?php echo $hackathon->getDateDebut() ?></td>	
-				</tr>
-			<?php } ?>
-		</table>
-	</main>
+    <h2>Accueil : à faire !</h2>
+    <section>
+        <h3>Voici la liste des nouveaux hackathons : inscrivez vous !</h3>
+    </section>
+    <?php if (!empty($hackathons)) { ?>
+        <table border>
+            <tr>
+                <th>id</th>
+                <th>nom</th>
+                <th>date de début</th>
+            </tr>
+            <?php foreach ($hackathons as $hackathon) { ?>
+                <tr>
+                    <td><?php echo $hackathon->getId() ?></td>
+                    <td><?php echo $hackathon->getNom() ?></td>
+                    <td><?php echo $hackathon->getDateDebut() ?></td>
+                </tr>
+            <?php } ?>
+        </table>
+    <?php } ?><strong> <?php if(empty($hackathons)) {echo("Pas d'hackathons pour aujourd'hui"); } ?></strong>
+
+</main>
 	<footer>
 		<?php include("./views/include/footer.php"); ?>
 	</footer>
 </body>
 
 </html>
+
+
